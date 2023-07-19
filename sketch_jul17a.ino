76fdf2c220b3045e24b6fae0b866a3b81da14a24
guffoe <guffoe@gmail.com>
2023-07-19 19:38:55 +0800

    Update sketch_jul17a.ino
    2nd update : add buzzer tone MQ5
                            define pin

--- sketch_jul17a.ino
+++ sketch_jul17a.ino
@@ -1,16 +1,18 @@
 #include "CTBot.h"
 CTBot myBot;
 
-String ssid = "m03";     // dengan nama wifi anda
-String pass = "m03guffoe"; // password wifi
-String token = "6152483448:AAE7ZYs-wBpxn-ZuCJNaDEU6u02vJDnz1-M"; // token bot telegram
+String ssid = "m03";     // Sesuikan dengan nama wifi anda
+String pass = "m03guffoe"; // sesuaikan password wifi
+String token = "6152483448:AAE7ZYs-wBpxn-ZuCJNaDEU6u02vJDnz1-M"; // token bot telegram yang telah dibuat
 
-#define Fire 12 //GPIO12 (D6)
-#define Smoke 14 //GPIO14 (D5)
+#define Api 12 //GPIO12 (D6)
+#define Gas 14 //GPIO14 (D5)
+#define buzzer 15 //GPIO15 (D8)
 
 void setup() {
-  pinMode(Fire, INPUT);
-  pinMode(Smoke, INPUT);
+  pinMode(Api, INPUT);
+  pinMode(Gas, INPUT);
+  pinMode(buzzer,OUTPUT);
   Serial.begin(115200);
   myBot.wifiConnect(ssid, pass);
 
@@ -18,31 +20,33 @@ void setup() {
 
   // check if all things are ok
   if (myBot.testConnection())
-    Serial.println("\n Connected");
+    Serial.println("\n Terhubung");
   else
-    Serial.println("\n Disconnected");
+    Serial.println("\n Tidak Terhubung");
 }
-  
+
 void loop() {
-  int readfire = digitalRead(Fire);
-  int readsmoke = digitalRead(Smoke);
-  Serial.print("Fire : ");
-  Serial.print(readfire);
-  Serial.print("   Smoke : ");
-  Serial.println(readsmoke);
-  if(readsmoke==0) //detectection smoke
+  int bacasensorapi = digitalRead(Api);
+  int bacasensorgas = digitalRead(Gas);
+  Serial.print("Api : ");
+  Serial.print(bacasensorapi);
+  Serial.print("   Gas : ");
+  Serial.println(bacasensorgas);
+  if(bacasensorgas==0) //terdeteksi gas
   {
     String kirim;
-    kirim ="Lapor Bos.! Ada Kebocoran Gas";
-    myBot.sendMessage(1186012126, kirim); 
-    delay(500); 
+    kirim ="Warning bossku..! Ada Kebocoran Gas";
+    myBot.sendMessage(724268164, kirim);
+    digitalWrite(buzzer,HIGH); // triggers the buzzer
+    delay(1000);
   }
-  if(readfire==0) //detectection fire
+  if(bacasensorapi==0) //terdeteksi api
   {
     String kirim;
-    kirim ="Lapor bos.! Ada Kebakaran";
-    myBot.sendMessage(1186012126, kirim); 
-    delay(500); 
+    kirim ="Warning bossku..! Ada Kebakaran";
+    myBot.sendMessage(724268164, kirim);
+    digitalWrite(buzzer,HIGH); // triggers the buzzer
+    delay(1000);
   }
-  delay(500);
+    delay(1000);
 }
